e25f9e081953d613089a27f37e9386ed58772afd
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-10-07 14:09:57 +0700

    Add files via upload

---
+++ tugas3-1.php
@@ -0,0 +1,13 @@
+<?php
+$j = 360;
+$k = 90;
+
+$w = $j/$k*60;
+
+
+print ("Jarak kota A - Z = ".$j." Km <br>");
+print ("Kecepatan Motor = ".$k."km/jam <br>");
+print ("Waktu Tempuh = ?<br>");
+print ("Jawaban : ".$w."Menit");
+
+?>
---
+++ tugas3-2.php
@@ -0,0 +1,13 @@
+<?php
+$a = 150000;
+$b = 12.5 ;
+
+$c = $a+($a*$b/100);
+
+
+print ("Tabungan Awal = Rp ".$a."<br>");
+print ("Bunga Setahun = ".$b." % <br>");
+print ("Total Tabungan Setahun = ?<br>");
+print ("Jawaban : Rp ".$c);
+
+?>
